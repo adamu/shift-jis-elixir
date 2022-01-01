b1760c330b1d1d07b5f35881e35e2af8359c3cfa
Adam Millerchip <adam@millerchip.net>
2022-01-01 18:41:37 +0900

    mix new shift_jis

---
+++ lib/shift_jis.ex
@@ -0,0 +1,18 @@
+defmodule ShiftJis do
+  @moduledoc """
+  Documentation for `ShiftJis`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ShiftJis.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/shift_jis_test.exs
@@ -0,0 +1,8 @@
+defmodule ShiftJisTest do
+  use ExUnit.Case
+  doctest ShiftJis
+
+  test "greets the world" do
+    assert ShiftJis.hello() == :world
+  end
+end
